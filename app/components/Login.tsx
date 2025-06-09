4f64b30b6267726b94f0a50a1271d5fa4db5a42d
emircorp <141232565+emircorp@users.noreply.github.com>
2025-06-09 15:01:08 +0300

    Update Login.tsx

--- app/components/Login.tsx
+++ app/components/Login.tsx
@@ -9,7 +9,7 @@ function Login({}: Props) {
     <div className="bg-white h-screen flex flex-col items-center justify-center text-center">
       <div className="mb-28">
         <h3 className="text-[#2D333A] text-xl sm:text-3xl font-semibold">Welcome to brAIn</h3>
-        <p className="flex text-sm font-medium text-gray-600 -mt-1 justify-end">created by Muhammed Emir Baysal & Jean Haddad<a className="underline hover:text-black" href="https://brain-ai.online" target="_blank" rel="noopener noreferrer">www.developercc.com</a></p>
+        <p className="flex text-sm font-medium text-gray-600 -mt-1 justify-end">created by Muhammed Emir Baysal & Jean Haddad</p>
       </div>
       <button
         onClick={() => signIn("google")}
