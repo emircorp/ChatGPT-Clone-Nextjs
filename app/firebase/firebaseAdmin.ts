abbafe9af09b2df8e00f7d14f6a4a0193724aa20
emircorp <141232565+emircorp@users.noreply.github.com>
2025-06-10 20:56:38 +0300

    Update firebaseAdmin.ts

--- app/firebase/firebaseAdmin.ts
+++ app/firebase/firebaseAdmin.ts
@@ -1,16 +1,21 @@
+// app/firebase/firebaseAdmin.ts
+
 import admin from "firebase-admin";
 import { getApps } from "firebase-admin/app";
 
+// .env'den gelen service account key'i parse et
 const serviceAccount = JSON.parse(
   process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string
 );
 
+// Firebase admin uygulamasını başlat
 if (!getApps().length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://brain-ai-online-official.firebaseio.com", // Realtime DB URL
   });
 }
 
-const adminDb = admin.firestore();
-
+// Realtime Database instance'ı dışa aktar
+const adminDb = admin.database();
 export { adminDb };
