0c307cf4b133ebdb869c29c4add8cdc839bc5077
emircorp <141232565+emircorp@users.noreply.github.com>
2025-06-10 21:43:43 +0300

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,25 @@
 import NextAuth from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 import { NextAuthOptions } from "next-auth";
 
 export const authOptions: NextAuthOptions = {
   providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
+    GoogleProvider({
+      clientId: process.env.WEB_CLIENT_ID!,
+      clientSecret: process.env.WEB_CLIENT_KEY!,
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async session({ session, token }) {
+      session.user.username = session.user.name
+        ?.split(" ")
+        .join("")
+        .toLowerCase();
+      session.user.uid = token.sub;
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET!,
 };
 
 const handler = NextAuth(authOptions);
