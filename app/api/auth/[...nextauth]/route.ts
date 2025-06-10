9f87d0aaae36c4138fa610aa5f20767f66b48aed
emircorp <141232565+emircorp@users.noreply.github.com>
2025-06-10 21:47:43 +0300

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { NextAuthOptions } from "next-auth";
+import { authOptions } from "@/app/lib/authOptions";
 
 export const authOptions: NextAuthOptions = {
   providers: [
