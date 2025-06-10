925900a206ee2d2c3e3a0277998a7c36e3550a79
emircorp <141232565+emircorp@users.noreply.github.com>
2025-06-10 21:08:54 +0300

    Update Message.tsx

--- app/components/Message.tsx
+++ app/components/Message.tsx
@@ -1,15 +1,24 @@
 "use client";
 
-import { DocumentData } from "firebase/firestore";
 import { motion } from "framer-motion";
 import React from "react";
 
+type MessageType = {
+  text: string;
+  createdAt: number;
+  user: {
+    name: string;
+    email: string;
+    avatar: string;
+  };
+};
+
 type Props = {
-  message: DocumentData;
+  message: MessageType;
 };
 
 function Message({ message }: Props) {
-  const isChatGPT = message.user.name === "ChatGPT";
+  const isChatGPT = message.user.name === "brAIn";
 
   return (
     <motion.div
@@ -19,7 +28,11 @@ function Message({ message }: Props) {
       className={`py-5 text-white ${isChatGPT && "bg-[#434654]"}`}
     >
       <div className="flex items-center space-x-5 px-10 max-w-4xl mx-auto">
-        <img src={message.user.avatar} alt="" className="h-6 w-6 sm:h-10 sm:w-10 rounded-sm" />
+        <img
+          src={message.user.avatar}
+          alt=""
+          className="h-6 w-6 sm:h-10 sm:w-10 rounded-sm"
+        />
         <p className="pt-1 text-sm sm:text-base">{message.text}</p>
       </div>
     </motion.div>
