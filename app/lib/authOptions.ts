f2ceae1463c2fa6cf6722251ab0f58f3cecd1b37
emircorp <141232565+emircorp@users.noreply.github.com>
2025-06-10 21:47:15 +0300

    Create authOptions.ts

---
+++ app/lib/authOptions.ts
@@ -0,0 +1,21 @@
+import GoogleProvider from "next-auth/providers/google";
+import type { NextAuthOptions } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.WEB_CLIENT_ID!,
+      clientSecret: process.env.WEB_CLIENT_KEY!,
+    }),
+  ],
+  callbacks: {
+    async session({ session, token }) {
+      if (session?.user) {
+        session.user.username = session.user.name?.split(" ").join("").toLowerCase();
+        session.user.uid = token.sub;
+      }
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET!,
+};
